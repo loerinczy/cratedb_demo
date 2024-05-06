f5fd9427254ddd8ca57bd7a1af5b58b95906a54c
Döme Lörinczy <dome.lorinczy@gmail.com>
2024-05-06 16:52:23 +0200

    finalize chunk_and_embed.py

---
+++ hacker_news/chunk_and_embed.py
@@ -0,0 +1,75 @@
+from utils import (
+    get_create_table_stmt,
+    get_insert_stmt,
+    load_snapshot,
+    Tables,
+    Schemas,
+    dict2tuple,
+    tuple2dict,
+    get_connection,
+    get_batches,
+    get_col,
+    chunk_html,
+)
+from ai_client import AIClient
+
+CHUNK_SIZE = 600
+CHUNK_OVERLAP = 60
+BATCH_SIZE = 20
+
+client = AIClient(
+    gpt_model="gpt-3.5-turbo-0125", embedding_model="text-embedding-3-small"
+)
+
+stories_text = load_snapshot("stories_text")
+stories_text = tuple2dict(stories_text, Schemas.stories_text)
+
+# TODO: experiment
+headers_to_split_on = [
+    ("h1", "Header 1"),
+    ("h2", "Header 2"),
+    ("h3", "Header 3"),
+    ("h4", "Header 4"),
+    ("h5", "Header 5"),
+    ("h6", "Header 6"),
+]
+insert_records = []
+
+num_records = len(stories_text)
+
+for idx, text_row in enumerate(stories_text):
+    html = text_row["text"]
+    chunks = chunk_html(
+        html,
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP,
+        headers_to_split_on=headers_to_split_on,
+    )
+    # in case of chunking does not return sensible data
+    if not chunks:
+        continue
+    chunks = [chunk.page_content for chunk in chunks]
+    embeddings = client.embed(chunks)
+    for chunk, embedding in zip(chunks, embeddings):
+        insert_row = text_row.copy()
+        insert_row["chunk"] = chunk
+        insert_row["embedding"] = embedding
+        insert_records.append(insert_row)
+    print(f"Chunked and embedded the {idx + 1}. story out of {num_records}.", end="\r")
+
+insert_records = dict2tuple(insert_records, Schemas.stories_chunk)
+create_table_stmt = get_create_table_stmt(Tables.stories_chunk, Schemas.stories_chunk)
+insert_stmt = get_insert_stmt(Tables.stories_chunk, Schemas.stories_chunk)
+num_batches = -(len(insert_records) // -BATCH_SIZE)
+
+with get_connection() as conn:
+    cursor = conn.cursor()
+
+    # create target table
+    cursor.execute(create_table_stmt)
+
+    # insert data in batches
+    batches = get_batches(insert_records, BATCH_SIZE)
+    for idx, batch in enumerate(batches):
+        cursor.executemany(insert_stmt, batch)
+        print(f"Inserted the {idx+1}. batch out of {num_batches}.", end="\r")
