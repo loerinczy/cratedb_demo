93cb47ad86611135ea3999fc279dee0d6e97b360
Döme Lörinczy <dome.lorinczy@gmail.com>
2024-06-03 15:00:54 +0200

    clean up old code

--- hacker_news/utils/catalog.py
+++ hacker_news/utils/catalog.py
@@ -2,35 +2,6 @@ from collections import OrderedDict
 
 
 class Schemas:
-    stories = OrderedDict(
-        [
-            ("deleted", "boolean"),
-            ("dead", "boolean"),
-            ("parent", "int"),
-            ("poll", "int"),
-            ("parts", "array (int)"),
-            ("author", "text"),
-            ("descendants", "int"),
-            ("id", "int"),
-            ("kids", "array (int)"),
-            ("score", "int"),
-            ("time", "bigint"),
-            ("title", "text"),
-            ("type", "text"),
-            ("url", "text"),
-        ]
-    )
-    stories_text = OrderedDict(
-        [
-            ("author", "text"),
-            ("id", "integer"),
-            ("score", "integer"),
-            ("time", "bigint"),
-            ("title", "text"),
-            ("url", "text"),
-            ("text", "text"),
-        ]
-    )
     stories_chunk = OrderedDict(
         [
             ("author", "text"),
@@ -56,6 +27,4 @@ class Table:
 
 
 class Tables:
-    stories = Table("hackernews.stories", Schemas.stories)
-    stories_text = Table("hackernews.stories_text", Schemas.stories_text)
     stories_chunk = Table("hackernews.stories_chunk", Schemas.stories_chunk)
